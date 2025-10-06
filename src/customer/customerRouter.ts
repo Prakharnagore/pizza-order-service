7e940d966c2a86f6a6ca507df341ea3c9e21656b
Prakhar Nagore <prakharnagore@Prakhars-MacBook-Air.local>
2025-10-07 00:32:55 +0530

    add address

---
+++ src/customer/customerRouter.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { CustomerController } from "./customerController";
+import { asyncWrapper } from "../utils";
+import authenticate from "../common/middleware/authenticate";
+
+const router = express.Router();
+
+const customerController = new CustomerController();
+
+router.get("/", authenticate, asyncWrapper(customerController.getCustomer));
+
+router.patch(
+  "/addresses/:id",
+  authenticate,
+  asyncWrapper(customerController.addAddress),
+);
+export default router;
