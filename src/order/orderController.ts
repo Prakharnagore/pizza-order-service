2fb2d906804d2f0d8919633374ec4ff33e90724c
Prakhar Nagore <prakharnagore@Prakhars-MacBook-Air.local>
2025-10-11 23:04:20 +0530

    create-order-api

---
+++ src/order/orderController.ts
@@ -0,0 +1,146 @@
+import { NextFunction, Request, Response } from "express";
+import {
+  CartItem,
+  ProductPricingCache,
+  Topping,
+  ToppingPriceCache,
+} from "../types";
+import productCacheModel from "../productCache/productCacheModel";
+import toppingCacheModel from "../toppingCache/toppingCacheModel";
+import couponModel from "../coupon/couponModel";
+
+export class OrderController {
+  constructor() {}
+
+  create = async (req: Request, res: Response, next: NextFunction) => {
+    const {
+      cart,
+      couponCode,
+      tenantId,
+      paymentMode,
+      customerId,
+      comment,
+      address,
+    } = req.body;
+    // todo: validate request data.
+
+    const totalPrice = await this.calculateTotal(cart);
+
+    let discountPercentage = 0;
+
+    if (couponCode) {
+      discountPercentage = await this.getDiscountPercentage(
+        couponCode,
+        tenantId,
+      );
+    }
+
+    const discountAmount = Math.round((totalPrice * discountPercentage) / 100);
+
+    const priceAfterDiscount = totalPrice - discountAmount;
+
+    // todo: May be store in db for each tenant.
+    const TAXES_PERCENT = 18;
+
+    const taxes = Math.round((priceAfterDiscount * TAXES_PERCENT) / 100);
+    // todo: may be store in database for each tenant or maybe calculated according to business rules.
+    const DELIVERY_CHARGES = 100;
+
+    const finalTotal = priceAfterDiscount + taxes + DELIVERY_CHARGES;
+
+    return res.json({ finalTotal });
+  };
+
+  private calculateTotal = async (cart: CartItem[]) => {
+    const productIds = cart.map((item) => item._id);
+
+    const productPricings = await productCacheModel.find({
+      productId: {
+        $in: productIds,
+      },
+    });
+
+    // todo: What will happen if product does not exists in the cache
+    // 1. call catalog service.
+    // 2. Use price from cart <- BAD
+
+    const cartToppingIds = cart.reduce((acc, item) => {
+      return [
+        ...acc,
+        ...item.chosenConfiguration.selectedToppings.map(
+          (topping) => topping.id,
+        ),
+      ];
+    }, []);
+
+    // todo: What will happen if topping does not exists in the cache
+    const toppingPricings = await toppingCacheModel.find({
+      toppingId: {
+        $in: cartToppingIds,
+      },
+    });
+
+    const totalPrice = cart.reduce((acc, curr) => {
+      const cachedProductPrice = productPricings.find(
+        (product) => product.productId === curr._id,
+      );
+
+      return (
+        acc +
+        curr.qty * this.getItemTotal(curr, cachedProductPrice, toppingPricings)
+      );
+    }, 0);
+
+    return totalPrice;
+  };
+
+  private getItemTotal = (
+    item: CartItem,
+    cachedProductPrice: ProductPricingCache,
+    toppingsPricings: ToppingPriceCache[],
+  ) => {
+    const toppingsTotal = item.chosenConfiguration.selectedToppings.reduce(
+      (acc, curr) => {
+        return acc + this.getCurrentToppingPrice(curr, toppingsPricings);
+      },
+      0,
+    );
+
+    const productTotal = Object.entries(
+      item.chosenConfiguration.priceConfiguration,
+    ).reduce((acc, [key, value]) => {
+      const price =
+        cachedProductPrice.priceConfiguration[key].availableOptions[value];
+      return acc + price;
+    }, 0);
+
+    return productTotal + toppingsTotal;
+  };
+
+  private getCurrentToppingPrice = (
+    topping: Topping,
+    toppingsPricings: ToppingPriceCache[],
+  ) => {
+    return toppingsPricings.find((t) => t.toppingId === topping.id)?.price || 0;
+  };
+
+  private getDiscountPercentage = async (
+    couponCode: string,
+    tenantId: string,
+  ) => {
+    const code = await couponModel.findOne({ code: couponCode, tenantId });
+
+    if (!code) {
+      return 0;
+    }
+
+    const currentDate = new Date();
+    const couponDate = new Date(code.validUpto);
+
+    if (currentDate <= couponDate) {
+      return code.discount;
+    }
+
+    return 0;
+  };
+}
